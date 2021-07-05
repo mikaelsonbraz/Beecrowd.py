09fb0d8f930d02e9ddfe892098d1cbd9d8e2b178
Mikaelson Braz <mikaelsonmrn@gmail.com>
2021-07-05 12:26:09 -0300

    matriiiiiiizes

---
+++ iniciante/1181.py
@@ -0,0 +1,20 @@
+l = int(input())
+t = input()
+
+tamanho = 12
+matriz = []
+for i in range(tamanho):
+    linha = []
+    for j in range(tamanho):
+        linha.append(float(input()))
+    matriz.append(linha)
+
+soma = 0
+for v in matriz[l]:
+    soma += v
+
+resultado = soma
+if t == 'M':
+    resultado = soma / tamanho
+
+print(f'{resultado:.1f}')
