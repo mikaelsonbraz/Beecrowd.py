bd9755749a49f69271f500961dd7654661d2a31e
Mikaelson Braz <mikaelsonmrn@gmail.com>
2021-07-08 11:12:10 -0300

    menino, eu sou o rei do raciocínio lógico hahaaaay

---
+++ iniciante/1187.py
@@ -0,0 +1,21 @@
+soma_media = input().title()
+
+matriz = []
+soma = 0
+nuns = 0
+for x in range(12):
+    matrizX = []
+    for y in range(12):
+        entrada = float(input())
+        matrizX.append(entrada)
+        if x <= 4 and y > x and y + x <= 10:
+            soma += entrada
+            nuns += 1
+        else:
+            pass
+    matriz.append(matrizX)
+
+if soma_media == 'S':
+    print(f"{soma:.1f}")
+elif soma_media == 'M':
+    print(f"{soma / nuns:.1f}")
