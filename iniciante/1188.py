a66fedfdf9600dff08eff870772a893b512d9e4c
Mikaelson Braz <mikaelsonmrn@gmail.com>
2021-07-08 11:18:24 -0300

    Mais dois testes de matrizes

---
+++ iniciante/1188.py
@@ -0,0 +1,21 @@
+soma_media = input().title()
+
+matriz = []
+soma = 0
+nuns = 0
+for x in range(12):
+    matrizX = []
+    for y in range(12):
+        entrada = float(input())
+        matrizX.append(entrada)
+        if x >= 7 and x + y >= 12 and x > y:
+            soma += entrada
+            nuns += 1
+        else:
+            pass
+    matriz.append(matrizX)
+
+if soma_media == 'S':
+    print(f"{soma:.1f}")
+elif soma_media == 'M':
+    print(f"{soma / nuns:.1f}")
---
+++ iniciante/1189.py
@@ -0,0 +1,21 @@
+soma_media = input().title()
+
+matriz = []
+soma = 0
+nuns = 0
+for x in range(12):
+    matrizX = []
+    for y in range(12):
+        entrada = float(input())
+        matrizX.append(entrada)
+        if x > y and x + y <= 10:
+            soma += entrada
+            nuns += 1
+        else:
+            pass
+    matriz.append(matrizX)
+
+if soma_media == 'S':
+    print(f"{soma:.1f}")
+elif soma_media == 'M':
+    print(f"{soma / nuns:.1f}")
